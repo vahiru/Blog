201c829299874461790d89413956fac782cea6f9
vahiru <nekovahiru@outlook.com>
2025-11-25 01:54:55 +0800

    add friends

--- src/data/friends.ts
+++ src/data/friends.ts
@@ -9,7 +9,13 @@ export interface Friend {
 }
 
 export const friendsData: Friend[] = [
-  { 
+    { 
+    name: "風雪城", 
+    desc: "浩繁星空下的一场稚嫩的梦", 
+    avatar: "https://imgsrc.chyk.ink/8mQ9M-dZdj9pJaTx.webp",
+    url: "https://blog.chyk.ink/"
+    },
+    { 
     name: "MBRjun-Blog", 
     desc: "我们生活在大地上，但我们的梦想超越天空", 
     avatar: "https://lfs.libmbr.com/assets/pics/LG4v5Ravatar180px.webp",
