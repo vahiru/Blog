ab7bfb3e37b71b57275e39c62c3372583a007909
vahiru <nekovahiru@outlook.com>
2025-12-28 02:35:15 +0800

    基本功能算是修好了

--- astro.config.ts
+++ astro.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from "astro/config";
 import MochiTones from "astro-mochi-tones";
+import mdx from "@astrojs/mdx";
 
 export default defineConfig({
     site: 'https://vahiru.is-cute.cat/',
     integrations: [
+        mdx(),
         MochiTones({
             config: {
                 title: "Vahiru Blog",
